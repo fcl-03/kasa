31098895f21bcb1c8ff3a8144afbf241a642bff1
fcl-03 <clementineflorian03@gmail.com>
2026-01-06 17:45:54 +0100

    page error 404 fonctionnelle et correction police

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,10 +1,16 @@
-function Error() {
-  return (
-    <main>
-      <h1>404</h1>
-      <p>Oups! La page que vous demandez n'existe pas.</p>
-    </main>
-  )
-}
+import { Link } from "react-router-dom";
+import "./Error.scss";
 
-export default Error
+export default function Error() {
+  return (
+    <main className="error">
+      <h1 className="error__code">404</h1>
+
+      <p className="error__text">Oups! La page que vous demandez n&apos;existe pas.</p>
+
+      <Link to="/" className="error__link">
+        Retourner sur la page d’accueil
+      </Link>
+    </main>
+  );
+}
